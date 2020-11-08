ed584caea7d61d0030bd9721c964df17c5b46285
parazsl <laccnow@gmail.com>
2020-11-08 20:27:45 +0100

    Inline svg for gh-pages

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,5 @@
+var closeImg =
+  '<svg width="8" height="8" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 .8L7.2 0 4 3.2.8 0 0 .8 3.2 4 0 7.2l.8.8L4 4.8 7.2 8l.8-.8L4.8 4 8 .8z" fill="#050038"/></svg>';
 var el = document.querySelector('#emails');
 var addButton = document.querySelector('#button-add');
 var countButton = document.querySelector('#button-count');
@@ -5,7 +7,7 @@ var countButton = document.querySelector('#button-count');
 var inputOptions = {
   element: el,
   placeholderText: 'add more people...',
-  removeContent: '<img src="close.svg">',
+  removeContent: closeImg,
 };
 
 var emails = new emailsInput(inputOptions);
